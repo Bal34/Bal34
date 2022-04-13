38317f1a111c94be8a6eb8711e49c244f471b258
Bal34 <102827609+Bal34@users.noreply.github.com>
2022-04-13 02:46:32 -0400

    Add files via upload

---
+++ Validate.js
@@ -0,0 +1,15 @@
+function dynamic()
+{
+if(document.getElementById('hiring').checked)
+{
+document.getElementById('hiring').style.display = "block";
+}
+else
+{
+document.getElementById('hiring').style.display = "none";
+}
+return;
+}
+  
+
+  
