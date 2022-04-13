5976d847173373b928a115f6e9cad621cd4700ab
Bal34 <102827609+Bal34@users.noreply.github.com>
2022-04-13 04:31:21 -0400

    Update Validate.js

--- Validate.js
+++ Validate.js
@@ -1,15 +1,34 @@
-function dynamic()
-{
-if(document.getElementById('hiring').checked)
-{
-document.getElementById('hiring').style.display = "block";
-}
-else
-{
-document.getElementById('hiring').style.display = "none";
-}
-return;
-}
+"use strict";
+var optCheck = { 
+  init: function() {
+    this.applyConditionalRequired();
+    this.bindUIActions();
+  },
+  
+  bindUIActions: function() {
+    $("input[type='radio'], input[type='checkbox']").on("change", this.applyConditionalRequired);
+  },
+  
+  applyConditionalRequired: function() {
+    
+    $(".require-if-active").each(function() {
+      var el = $(this);
+      if ($(el.data("require-pair")).is(":checked")) {
+        el.prop("required", true);
+      } else {
+        el.prop("required", false);
+      }
+    });
+    
+  }  
+};
+optCheck.init();
+
+window.onload = function () {
+  const form = document.querySelector("form");
+  form.onsubmit = validate;
+
+};
   
 
   
